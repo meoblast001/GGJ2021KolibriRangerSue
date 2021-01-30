d5ada1c772934f1c84f81dc2fcd132d337757e07
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-01-30 14:01:28 +0100

    Roof.

---
+++ Assets/ActivateOnAwake.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnAwake : MonoBehaviour
+{
+    void Awake()
+    {
+        this.gameObject.active = true;
+    }
+}
