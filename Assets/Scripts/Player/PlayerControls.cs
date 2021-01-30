316f704fdc1e687b8fba28e28b4a75dc29045209
Braden Walters <vc@braden-walters.info>
2021-01-30 11:55:37 +0100

    Create PlayerControls component that reacts to the player controls

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public PlayerInput _input;
+
+    private void Awake()
+    {
+        Debug.Log($"Waking up {nameof(PlayerControls)}");
+        _input = new PlayerInput();
+        _input.PlayerStandard.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        _input.PlayerStandard.Interact.performed += _ => Interact();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void Move(Vector2 direction)
+    {
+        Debug.Log($"Moving in direction {direction}");
+    }
+
+    private void Interact()
+    {
+        Debug.Log("Interacting");
+    }
+}
