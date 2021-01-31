dea29366ac2921f8e21ff6fa253f6da609b74e45
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-01-31 13:06:26 +0100

    Effect.

--- Assets/Scripts/Effect.cs
+++ Assets/Scripts/Effect.cs
@@ -1,9 +1,21 @@
+using System;
 using UnityEngine;
 
+public enum EffectKind
+{
+    Catnip
+}
 public class Effect: MonoBehaviour
 {
+    [SerializeField] private EffectKind kind;
     public void OnEffect()
     {
-
+        switch (kind)
+        {
+            case EffectKind.Catnip:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
